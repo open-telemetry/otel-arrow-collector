495178aa069fc8c8520ce80e0569e30101708427
Joshua MacDonald <jmacd@lightstep.com>
2022-11-18 09:10:39 -0800

    lint

--- receiver/otlpreceiver/internal/arrow/arrow.go
+++ receiver/otlpreceiver/internal/arrow/arrow.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package arrow
+package arrow // import "go.opentelemetry.io/collector/receiver/otlpreceiver/internal/arrow"
 
 import (
 	"context"
